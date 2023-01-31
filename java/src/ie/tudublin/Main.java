4f5e086db9de60fd50e740d2df438b10c188786e
Caoimhin <caoimhinecondon@gmail.com>
2023-01-31 15:11:11 +0000

    killing the cat

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -16,6 +16,12 @@ public class Main
 		Dog penny = new Dog();
 		penny.setName("Penny");
 		penny.speak();
-	}
-	
+
+		cat ginger = new cat("Ginger");
+
+		while(ginger.life == true){
+
+			ginger.kill();
+		}
+	}	
 }
--- java/src/ie/tudublin/cat.java
+++ java/src/ie/tudublin/cat.java
@@ -3,9 +3,34 @@ package ie.tudublin;
 public class cat {
     
     int numLives = 9;
+    String name;
+    boolean life = true;
 
-    public void meow(){
+    public void setName(String name){
 
+        this.name = name;
+    }
+
+    public cat(){
         
+    }
+
+    public cat(String name){
+
+    }
+
+    public void kill (){
+
+        numLives--;
+
+        if (numLives > 0){
+
+            System.out.println("Ouch");
+        }
+        else {
+
+            life = false;
+            System.out.println("Dead");
+        }
     }
 }
